8b10718a27696ac7a7d43128b70f5f17b5639026
Alex Viki <shevashokc@gmail.com>
2018-09-13 17:55:18 +0300

    created function for getting data from git

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-document.getElementById("btn_get_repos").onclick = function() {
-    $.ajax({
-        type: "GET",
-        url: "https://api.github.com/users/google/repos",
-        dataType: "json",
-        success: function(result) {
-            for( i in result ) {
-                $("#repo_list").append(
-                    "<li><a href='" + result[i].html_url + "' target='_blank'>" +
-                    result[i].name + "</a></li>"
-                );
-                //console.log("i: " + i);
-            }
-            console.log(result);
-            $("#repo_count").append("Total Repos: " + result.length);
-        }
-    });
+const inputValue = 'AlexViki';
+const clientId = 'Iv1.61d00f9cbaa368c9';
+const clientSecret = '4f227d4712c67f9ce756ed4b2b2bac2672093d99';
+
+document.getElementById("btn_get_repos").addEventListener('click', () => {
+    console.log('2222');
+    showData();
+} );
+
+const fetchUsers = async (user) => {
+    const api_call = await fetch('https://api.github.com/users/${user}?client_id=${clientId}&client_secret=${clientSecret}')
+
+    const datas = await api_call.json();
+
+    return {data: datas}; // return {data}; 
 };
+
+const showData = () => {
+    fetchUsers(inputValue).then((res) => {
+        console.log(res);
+    });
+}

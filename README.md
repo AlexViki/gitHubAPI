c4d01ca4e359bfb1a0d22832f43c9409a276daeb
Alex Viki <shevashokc@gmail.com>
2018-09-18 15:53:55 +0300

    Shows some info from Git's users.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gitHubAPI
+## gitHubAPI
+This my training app where I used AJAX request to Git service.
--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ document.getElementById("btn_get_repos").addEventListener('click', () => {
     getDate();
 } );
 
-let tmp = '';
-let avatar = "";
 let logo = document.getElementById('logo');
 let image = document.createElement("img");
 
@@ -14,15 +12,32 @@ const getDate = () =>{
         url: "https://api.github.com/users",
         dataType: "json",
         success: function(result) {
-            for( i in result ) {
-                console.log(result[i].login);
-                console.log(result[i].avatar_url);
-                tmp = tmp + result[i].login + "<br>";
-                document.getElementById('result').innerHTML = tmp;
-                image.src = result[i].avatar_url;
-                logo.appendChild(image);
-            }
-            console.log(result);
-            console.log(result[2].avatar_url);
+            //console.log(result.data);
+            drawUsers(result);
         }
 })}
+
+function drawUsers(users) {
+    let html = '';
+    let us = '';
+    let arlavatarImage = '';
+    //console.log(users);
+    for (i in users) {
+        us = users[i].login;
+        arlavatarImage = users[i].avatar_url;
+        //console.log(users[i].login);
+        //console.log(users[i]);
+        html += '<div class="list-group-item">'
+                    + '<a target="blank" href="https://github.com/' + users[i].login + '">'
+                        + '<img class="img-thumbnail" src="' + users[i].avatar_url + '">'
+                    + '</a>'
+                        + '<span>login: </span>'
+                            + '<a target="blank" href="https://github.com/' + users[i].login + '">'
+                        + users[i].login
+                    + '</a>'
+                    + '<p class="name-item">Name: </p>'
+                    + '<p>email: </p>'
+                + '</div>';
+    }
+    $('.item-list').html(html);
+}
